b28e1d28b3b95f55871751ecd551fe48da67550e
Yeahhooo <3266325505@qq.com>
2023-12-20 17:06:32 +0800

    feat: 我要加盟

---
+++ src/pages/join/components/introduceBar/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="sellIconContainer">
+    <div class="sellIconTitle">
+      <p>{{props.title}}</p>
+    </div>
+    <div  class="sellIconContent">
+      <p>{{props.content}}</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  title: String,
+  content: String
+})
+</script>
+
+<style scoped>
+.sellIconContainer{
+  width: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.sellIconTitle{
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  font-size: 30px;
+  font-weight: bold;
+  color: #eda01f;
+}
+
+.sellIconContent{
+  text-align: center;
+  width: 250px;
+  margin-top: -20px;
+  color: #a6a6a6;
+}
+</style>
